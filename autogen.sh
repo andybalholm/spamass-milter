96fc732fafe51c70d0aaf70a7ff71cd7e31eb99f
dnelson <dnelson>
2002-11-15 07:17:18 +0000

    Try really hard to run a new-ish version of autoconf.  This should make rhautogen.sh obsolete.

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,19 @@
 #!/bin/sh -e
-# $Id: autogen.sh,v 1.3 2002/07/24 16:19:53 dnelson Exp $
+# $Id: autogen.sh,v 1.4 2002/11/15 07:17:18 dnelson Exp $
 # Run this to update & generate all the automatic things
 #
 
+# hack because some OSes (cough RedHat cough) default to 2.13 even
+# though a perfectly good 2.5x is available
+AC=
+for i in 255 -2.55 2.55 254 -2.54 2.54 253 -2.53 2.53 ; do
+ if which autoconf$i ; then 
+  AC=$i ; break
+ fi
+done
 aclocal
-autoheader
+autoheader$AC
 automake --add-missing
-autoconf
+autoconf$AC
 
 ./configure $*
