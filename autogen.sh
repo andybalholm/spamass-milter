60306715af88c7a0b54c1793b22336460cc49996
dnelson <dnelson>
2003-03-10 23:42:07 +0000

    expand check to search for automake as well, check for more versions

--- autogen.sh
+++ autogen.sh
@@ -1,19 +1,26 @@
 #!/bin/sh -e
-# $Id: autogen.sh,v 1.4 2002/11/15 07:17:18 dnelson Exp $
+# $Id: autogen.sh,v 1.5 2003/03/10 23:42:07 dnelson Exp $
 # Run this to update & generate all the automatic things
 #
 
 # hack because some OSes (cough RedHat cough) default to 2.13 even
 # though a perfectly good 2.5x is available
 AC=
-for i in 255 -2.55 2.55 254 -2.54 2.54 253 -2.53 2.53 ; do
- if which autoconf$i ; then 
-  AC=$i ; break
+for i in 257 -2.57 256 -2.56 255 -2.55 2.55 254 -2.54 2.54 253 -2.53 2.53 ; do
+ if type autoconf$i >/dev/null 2>&1 ; then 
+  AC=$i ; echo detected autoconf$AC ; break
  fi
 done
-aclocal
+AM=
+for i in 17 -1.7 1.6 -1.6 15 -1.5 ; do
+ if type automake$i >/dev/null 2>&1 ; then 
+  AM=$i ; echo detected Automake$AM ; break
+ fi
+done
+
+aclocal$AM
 autoheader$AC
-automake --add-missing
+automake$AM --add-missing
 autoconf$AC
 
 ./configure $*
